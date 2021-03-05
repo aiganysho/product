2218a6b1152434ce7f430ecd816aa531bbbdc776
Aiganysh <aiganyshx@gmail.com>
2021-03-05 16:29:02 +0600

    create detailed view for page

--- hello/webapp/views.py
+++ hello/webapp/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
+from webapp.models import Product, categories
 # Create your views here
 
+def list_product(request):
+    lists = Product.objects.all()
+    return render(request, 'list_product.html', context={'lists': lists})
+
+def view_product(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'view_product.html', context={'product': product})
